8997ab6049c1ed05cdd3557bc6dbb88dcc00f0ee
alvinAdler <alvinadlersampow@gmail.com>
2021-10-28 23:04:02 +0700

    Added responsiveness to the main container and the profile component

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -7,8 +7,10 @@ const ProfileCard = ({changeSelectedTime}) => {
         <div className="profile-card-container">
             <div className="profile">
                 <img className="profile-picture" src="/images/image-jeremy.png" alt="Not available"/>
-                <p>Report for</p>
-                <p style={{fontWeight: 200}}>Jeremy <br></br> Robson</p>
+                <div className="profile-text-content">
+                    <p>Report for</p>
+                    <p style={{fontWeight: 200}}>Jeremy Robson</p>
+                </div>
             </div>
             <div className="time-navigation">
                 <p onClick={() => changeSelectedTime("daily")}>Daily</p>
